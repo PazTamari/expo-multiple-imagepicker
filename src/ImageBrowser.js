71e661ab52dc5de02181f9264c4da2d7b04041d5
pazta <paz268@gmail.com>
2019-01-30 12:10:35 +0200

    Video support

--- src/ImageBrowser.js
+++ src/ImageBrowser.js
@@ -6,11 +6,12 @@ import {
   CameraRoll,
   FlatList,
   Dimensions,
-  Button
+  Button,
 } from 'react-native';
 import { FileSystem } from 'expo';
 import ImageTile from './ImageTile';
-const { width } = Dimensions.get('window')
+import * as _ from 'lodash';
+const { width } = Dimensions.get('window');
 
 export default class ImageBrowser extends React.Component {
   constructor(props) {
@@ -19,65 +20,82 @@ export default class ImageBrowser extends React.Component {
       photos: [],
       selected: {},
       after: null,
-      has_next_page: true
-    }
+      has_next_page: true,
+    };
   }
 
   componentDidMount() {
-    this.getPhotos()
+    this.getPhotos();
   }
 
-  selectImage = (index) => {
-    let newSelected = {...this.state.selected};
+  selectImage = index => {
+    let newSelected = { ...this.state.selected };
     if (newSelected[index]) {
       delete newSelected[index];
     } else {
-      newSelected[index] = true
+      newSelected[index] = true;
     }
     if (Object.keys(newSelected).length > this.props.max) return;
     if (!newSelected) newSelected = {};
-    this.setState({ selected: newSelected })
-  }
+    this.setState({ selected: newSelected });
+  };
 
   getPhotos = () => {
-    let params = { first: 500, mimeTypes: ['image/jpeg', 'video/mp4'] };
-    if (this.state.after) params.after = this.state.after
-    if (!this.state.has_next_page) return
-    CameraRoll
-      .getPhotos(params)
-      .then(this.processPhotos)
-  }
+    let medias = [];
+    let mediaToProcess;
+    let photosParams = { first: 500, assetType: 'All' };
+    let videosParams = { first: 30, assetType: 'Videos' };
+    if (this.state.after) params.after = this.state.after;
+    if (!this.state.has_next_page) return;
+    CameraRoll.getPhotos(photosParams)
+      .then(photos => {
+        mediaToProcess = photos;
+        medias = _.slice(photos.edges, 0, 470);
+        return CameraRoll.getPhotos(videosParams);
+      })
+      .then(videos => {
+        mediaToProcess.edges = _.orderBy(
+          _.union(medias, videos.edges),
+          [elemnt => elemnt.node.timestamp],
+          ['desc'],
+        );
+        return mediaToProcess;
+      })
+      .then(this.processPhotos);
+  };
 
-  processPhotos = (r) => {
+  processPhotos = r => {
     if (this.state.after === r.page_info.end_cursor) return;
-    let uris = r.edges.map(i=> i.node).map(i=> i.image).map(i=>i.uri)
+    let uris = r.edges
+      .map(i => i.node)
+      .map(i => i.image)
+      .map(i => i.uri);
     this.setState({
       photos: [...this.state.photos, ...uris],
       after: r.page_info.end_cursor,
-      has_next_page: r.page_info.has_next_page
+      has_next_page: r.page_info.has_next_page,
     });
-  }
+  };
 
-  getItemLayout = (data,index) => {
-    let length = width/4;
-    return { length, offset: length * index, index }
-  }
+  getItemLayout = (data, index) => {
+    let length = width / 4;
+    return { length, offset: length * index, index };
+  };
 
   prepareCallback() {
     let { selected, photos } = this.state;
     let selectedPhotos = photos.filter((item, index) => {
-      return(selected[index])
+      return selected[index];
     });
-    let files = selectedPhotos
-      .map(i => FileSystem.getInfoAsync(i, {md5: true}))
-    let callbackResult = Promise
-      .all(files)
-      .then(imageData=> {
-        return imageData.map((data, i) => {
-          return {file: selectedPhotos[i], ...data}
-        })
-      })
-    this.props.callback(callbackResult)
+    let files = selectedPhotos.map(i =>
+      FileSystem.getInfoAsync(i, { md5: true }),
+    );
+    let callbackResult = Promise.all(files).then(imageData => {
+      return imageData.map((data, i) => {
+        return { file: selectedPhotos[i], ...data };
+      });
+    });
+    this.props.callback(callbackResult);
   }
 
   renderHeader = () => {
@@ -91,17 +109,14 @@ export default class ImageBrowser extends React.Component {
           onPress={() => this.props.callback(Promise.resolve([]))}
         />
         <Text>{headerText}</Text>
-        <Button
-          title="Done"
-          onPress={() => this.prepareCallback()}
-        />
+        <Button title="Done" onPress={() => this.prepareCallback()} />
       </View>
-    )
-  }
+    );
+  };
 
-  renderImageTile = ({item, index}) => {
-    let selected = this.state.selected[index] ? true : false
-    return(
+  renderImageTile = ({ item, index }) => {
+    let selected = this.state.selected[index] ? true : false;
+    return (
       <ImageTile
         item={item}
         index={index}
@@ -109,22 +124,24 @@ export default class ImageBrowser extends React.Component {
         selected={selected}
         selectImage={this.selectImage}
       />
-    )
-  }
+    );
+  };
   renderImages() {
-    return(
+    return (
       <FlatList
         data={this.state.photos}
         numColumns={4}
         renderItem={this.renderImageTile}
-        keyExtractor={(_,index) => index}
-        onEndReached={()=> {this.getPhotos()}}
+        keyExtractor={(_, index) => index}
+        onEndReached={() => {
+          this.getPhotos();
+        }}
         onEndReachedThreshold={0.5}
         ListEmptyComponent={<Text>Loading...</Text>}
         initialNumToRender={24}
         getItemLayout={this.getItemLayout}
       />
-    )
+    );
   }
 
   render() {
@@ -148,6 +165,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     padding: 10,
-    marginTop: 20
+    marginTop: 20,
   },
-})
+});
